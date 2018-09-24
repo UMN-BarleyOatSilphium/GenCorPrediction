1f7b6c8af23ea85f3f49f3a4365378f6d72ed31e
neyhartj <neyhartje@gmail.com>
2018-09-24 15:21:22 -0500

    Edit to bash

--- Scripts/Simulations/run_simulations.sh
+++ Scripts/Simulations/run_simulations.sh
@@ -2,23 +2,23 @@
 
 #PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
 # #PBS -l walltime=12:00:00,mem=62gb,nodes=1:ppn=16
-# #PBS -N PopVar_suitability_simulation
-# #PBS -N PopVar_gencor_selection_simulation
-#PBS -N PopVar_gencor_prediction_simulation
+# #PBS -N gencor_selection_simulation
+#PBS -N gencor_prediction_simulation
+# #PBS -N gencor_prediction_space_simulation
 #PBS -M neyha001@umn.edu
 #PBS -m abe
 #PBS -r n
 
 # Change the working directory
-cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/PopVarVal/Scripts/Simulations
+cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/GenCorPrediction/Scripts/Simulations
 
 module load R/3.5.0
 
-## Run the simulation
-# Rscript popvar_suitability_simulation.R
-
 # For genetic correlation
 Rscript popvar_gencor_simulation.R
+
+# For the genetic architecture space simulation
+Rscript popvar_gencor_space_simulation.R
 
 # For genetic correlation and selection
 # Rscript popvar_gencor_selection_simulation.R
