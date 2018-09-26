a76e74cdb388ff4c66106c5983bd3794675a7046
neyhartj <neyha001@umn.edu>
2018-09-26 16:40:26 -0500

    Edits to bash

--- Scripts/Simulations/run_simulations.sh
+++ Scripts/Simulations/run_simulations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # #PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
-#PBS -l walltime=36:00:00,mem=62gb,nodes=1:ppn=16
-# #PBS -N gencor_selection_simulation
-#PBS -N gencor_prediction_simulation
+#PBS -l walltime=12:00:00,mem=62gb,nodes=1:ppn=16
+#PBS -N gencor_selection_simulation
+# #PBS -N gencor_prediction_simulation
 # #PBS -N gencor_prediction_space_simulation
 #PBS -M neyha001@umn.edu
 #PBS -m abe
@@ -15,10 +15,10 @@ cd /panfs/roc/groups/6/smithkp/neyha001/Genomic_Selection/GenCorPrediction/Scrip
 module load R/3.5.0
 
 # For genetic correlation
-Rscript popvar_gencor_simulation.R
+# Rscript popvar_gencor_simulation.R
 
 # For the genetic architecture space simulation
 # Rscript popvar_gencor_space_simulation.R
 
 # For genetic correlation and selection
-# Rscript popvar_gencor_selection_simulation.R
+Rscript popvar_gencor_selection_simulation.R
