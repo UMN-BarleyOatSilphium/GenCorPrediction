dfc6f41665f2bc4d449a258d5a3ec54e85ec5944
neyhartj <neyha001@umn.edu>
2018-10-15 14:20:06 -0500

    bash changes

--- Scripts/Simulations/run_simulations.sh
+++ Scripts/Simulations/run_simulations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # #PBS -l walltime=24:00:00,mem=22gb,nodes=1:ppn=8
-#PBS -l walltime=30:00:00,mem=62gb,nodes=1:ppn=16
+#PBS -l walltime=30:00:00,mem=62gb,nodes=1:ppn=24
 #PBS -N gencor_selection_simulation
 # #PBS -N gencor_prediction_simulation
 # #PBS -N gencor_prediction_space_simulation
